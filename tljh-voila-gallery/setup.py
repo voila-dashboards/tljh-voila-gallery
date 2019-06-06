9a15f4cd48aa67e5fda4b05a47accf455b68a4af
yuvipanda <yuvipanda@gmail.com>
2019-06-06 16:13:16 +0200

    Fix typo in entrypoint

--- tljh-voila-gallery/setup.py
+++ tljh-voila-gallery/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="tljh-voila-gallery",
-    entry_points={"tljh": ["voila_gallery = voila_gallery"]},
+    entry_points={"tljh": ["tljh_voila_gallery = tljh_voila_gallery"]},
     py_modules=["tljh_voila_gallery"],
 )
 
