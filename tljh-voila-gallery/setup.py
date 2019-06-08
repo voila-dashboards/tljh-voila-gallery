44c86c3426c44fa370caa18f4a0bfce7f73e7c13
Pascal Bugnion <pascal@bugnion.org>
2019-06-08 06:31:04 +0000

    Add build-gallery-images entrypoint

--- tljh-voila-gallery/setup.py
+++ tljh-voila-gallery/setup.py
@@ -2,7 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="tljh-voila-gallery",
-    entry_points={"tljh": ["tljh_voila_gallery = tljh_voila_gallery"]},
+    entry_points={
+        "tljh": ["tljh_voila_gallery = tljh_voila_gallery"],
+        "console_scripts": ["build-gallery-images = build_gallery_images:main"]
+    },
     packages=find_packages(),
     include_package_data=True
 )
