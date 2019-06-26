04940e8eded8a539543b5c7f87b24e8a41f44d48
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2019-06-26 16:17:05 +0200

    Add console_scripts entry for install-build-units

--- tljh-voila-gallery/setup.py
+++ tljh-voila-gallery/setup.py
@@ -5,7 +5,10 @@ setup(
     version="0.0.1",
     entry_points={
         "tljh": ["tljh_voila_gallery = tljh_voila_gallery"],
-        "console_scripts": ["build-gallery-images = tljh_voila_gallery.build_images:main"]
+        "console_scripts": [
+            "build-gallery-images = tljh_voila_gallery.build_images:main",
+            "install-build-units = tljh_voila_gallery.install_build_units:main"
+        ]
     },
     packages=find_packages(),
     include_package_data=True,
