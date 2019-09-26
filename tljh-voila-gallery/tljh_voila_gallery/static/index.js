39d25133c68c1e053856e358877533716b4a3fc9
Sylvain Corlay <sylvain.corlay@gmail.com>
2019-09-26 19:54:56 +0200

    Make whole badge clickable

--- tljh-voila-gallery/tljh_voila_gallery/static/index.js
+++ tljh-voila-gallery/tljh_voila_gallery/static/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('.launch-example').click(function(event) {
+    $('.launch-item').click(function(event) {
         var example = $(event.currentTarget).data('example-name');
         $('#example-' + example).prop('checked', true);
         $('#spawn_form').submit();
